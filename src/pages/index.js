dac217c19e0694891b5e3e236596798727732024
ayan2207 <ayan.hedayati@holidayextras.com>
2019-09-27 00:00:50 +0100

    added bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React from "react"
+import { Container, Button, Jumbotron } from 'react-bootstrap'
 import { graphql, useStaticQuery } from 'gatsby'
 
-const IndexPage = () => {
+const IndexPage = () =>
+{
   const data = useStaticQuery(graphql`
   {
     site {
@@ -14,11 +16,15 @@ const IndexPage = () => {
   }
 `)
   return (
-    <div>
-      <h1>{data.site.siteMetadata.title}</h1>
-      <h2>{data.site.siteMetadata.tagline}</h2>
-      <p>This is v2 of this site split test</p>
-    </div>
+    <Container fluid>
+      <Jumbotron>
+        <h1>{data.site.siteMetadata.title}</h1>
+        <p>{data.site.siteMetadata.tagline}</p>
+        <p>
+          <Button variant="primary">Say Hi</Button>
+        </p>
+      </Jumbotron>
+    </Container>
   )
 }
 
