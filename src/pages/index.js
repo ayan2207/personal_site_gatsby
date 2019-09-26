fb998db8b7bde9ae226220d0627b349d7b800a76
ayan2207 <ayan.hedayati@holidayextras.com>
2019-09-26 22:47:11 +0100

    added paragraph

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,6 +17,7 @@ const IndexPage = () => {
     <div>
       <h1>{data.site.siteMetadata.title}</h1>
       <h2>{data.site.siteMetadata.tagline}</h2>
+      <p>This is v2 of this site split test</p>
     </div>
   )
 }
